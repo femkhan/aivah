9919634be714a8a67536be7807d454aea9c92ab4
femkhan <fem.iva@gmail.com>
2014-04-01 15:41:26 +0530

    404 Error Page
    Added String Translation

--- 404.php
+++ 404.php
@@ -12,9 +12,8 @@ get_header(); ?>
 		<div class="inner">	
 			<div class="entry-content">
 				<div class="error_404">
-					<h2>Ooops... Error 404</h2>
-					<h5>We're sorry, but the page you are looking for doesn't exist.</h5>
-					<p class="center"><a class="btn large magenta flat" href="<?php echo home_url(); ?>"><span>Go To Homepage</span></a></p>
+					<h5><?php _e('We\'re sorry, but the page you are looking for doesn\'t exist.', 'iva_frontend'); ?></h5>
+					<p class="center"><a class="btn large magenta flat" href="<?php echo home_url(); ?>"><span><?php _e('Go to Home', 'iva_frontend'); ?></span></a></p>
 				</div>			
 			</div><!-- .entry-content -->	
 		</div><!-- .inner -->
