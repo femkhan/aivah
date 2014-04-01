d4500486a9b88847f5cfd5847f86a658a2dcf48d
femkhan <fem.iva@gmail.com>
2014-04-01 15:30:45 +0530

    404 Error Page
    Error Page for WordPress

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<div id="primary" class="pagemid">
+	<div class="inner">	
+		<div class="entry-content">
+			<div class="error_404">
+				<h2>Ooops... Error 404</h2>
+				<h5>We're sorry, but the page you are looking for doesn't exist.</h5>
+				<p class="center"><a class="btn large magenta flat" href="<?php echo home_url(); ?>"><span>Go To Homepage</span></a></p>
+			</div>			
+		</div><!-- .entry-content -->
+	</div><!-- .inner -->
+</div><!-- .pagemid -->
+
+<?php get_footer(); ?>
